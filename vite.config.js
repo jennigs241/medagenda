b5728958f18df9895c598b20d7203f511488cc38
jennigs241 <ossiessejennigs@gmail.com>
2026-08-11 23:39:47 +0100

    fix: configuration finale pour le build automatique GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/medagenda/', // S'assurer que les deux slashes / sont présents
-})
+  base: '/medagenda/',
+});
